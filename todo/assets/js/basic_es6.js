caf219a5cb109761a403244c30c3c53c2319239d
dalingyu <dnps912@gmail.com>
2024-03-10 16:38:46 +0800

    0310todo

--- todo/assets/js/basic_es6.js
+++ todo/assets/js/basic_es6.js
@@ -42,5 +42,55 @@ const doFetch2 = async () => {
     console.log('doFetch2 run.');
 }
 
-doFetch2();
+const doFetchPost = async () => {
+    let api = 'https://book.niceinfos.com/frontend/api/';
+
+    let params = {
+        action: 'demo',
+        data: { a: 1, b: 2 },
+    };
+
+    let options = {
+        method: 'POST',
+        body: JSON.stringify(params)
+    };
+
+    let response = await fetch(api, options)
+    let data = await response.text();
+    data = JSON.parse(data);
+    console.log(data);
+    console.log(data.code);
+}
+
+const doUploadFile = async (file) => {
+    if (!file) {
+        return;
+    }
+
+    let api = 'https://book.niceinfos.com/frontend/api/';
+
+    let form = new FormData();
+    form.append('action', 'upload');
+    form.append('file', file);
+
+    let options = {
+        method: 'POST',
+        body: form
+    };
+
+    let response = await fetch(api, options);
+    let data = await response.text();
+    console.log(data);
+}
+
+let elFile = document.querySelector('#upload-file');
+let elBtn = document.querySelector('#updload-btn');
+
+elBtn.addEventListener('click', () => {
+    let file = elFile.files[0];
+    doUploadFile(file);
+})
+
+// doFetch2();
 // doGet();
+// doFetchPost();
