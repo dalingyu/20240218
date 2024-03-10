c46bb08d3ccb7a085dff37cf6b739bff28303826
dalingyu <dnps912@gmail.com>
2024-03-10 15:14:18 +0800

    0310

--- todo/assets/js/basic_es6.js
+++ todo/assets/js/basic_es6.js
@@ -1,3 +1,46 @@
 import { Todo } from './components/Todo.js';
 
 const app = new Todo();
+
+/**
+ * AJAX GET 發送
+ */
+const doGet = () => {
+    let request = new XMLHttpRequest();
+
+    request.addEventListener('load', () => {
+        console.log('loaded');
+        console.log(request.responseText);
+    })
+
+    request.open('GET', 'https://book.niceinfos.com/frontend/api/?action=sleep');
+    request.send();
+    console.log('doGet run.');
+}
+
+const doFetch = () => {
+    let api = 'https://book.niceinfos.com/frontend/api/?action=sleep';
+
+    fetch(api)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+        })
+
+    console.log('doFetch run.');
+}
+
+const doFetch2 = async () => {
+    let api = 'https://book.niceinfos.com/frontend/api/?action=sleep';
+
+    let response = await fetch(api);
+    let data = await response.text();
+
+    console.log(data);
+    console.log('doFetch2 run.');
+}
+
+doFetch2();
+// doGet();
